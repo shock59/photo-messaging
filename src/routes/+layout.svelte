dde564a6c9f1c51ba190d62cc5953dc2d73a830a
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-17 17:53:22 +1100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import '../style.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>Photo Messaging</title>
+	<link rel="icon" href={favicon} />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Andika:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+{@render children?.()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to Photo Messaging</h1>
+<p>paragraph text !!!</p>
