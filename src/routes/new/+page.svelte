98346ad3bc5db0b788ea8fdbe8ce2214b68e3bb2
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-18 08:15:58 +1100

    Improve search result display

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -1,23 +1,41 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	let query: string = $state('');
 
+	let searching: boolean = $state(false);
 	let images: Image[] = $state([]);
 
-	onMount(async () => {
-		const res = await fetch('./new/imageSearch/Earth');
+	async function search() {
+		if (!query.trim()) return;
+		searching = true;
+		const res = await fetch(`./new/imageSearch/${encodeURIComponent(query)}`);
 		images = await res.json();
-	});
+		searching = false;
+	}
 </script>
 
 <form method="POST" action="?/guess">
 	<input type="text" placeholder="Your message" name="text" />
 
-	<h2>Images</h2>
+	<div id="search-container">
+		<div class="row">
+			<input type="text" placeholder="Search images" bind:value={query} />
+			<button type="button" onclick={search}>Search</button>
+		</div>
 
-	<div id="images-container">
-		{#each images as image}
-			<img src={image.src} alt={image.alt} />
-		{/each}
+		<div id="images-container">
+			{#if searching}
+				Loading search results
+			{:else if images.length}
+				{#each images as image}
+					<img src={image.src} alt={image.alt} />
+				{/each}
+			{:else}
+				<span
+					>Tip: If you have a specific image on Wikimedia Commons you want to use, use the url for
+					its page as your search.</span
+				>
+			{/if}
+		</div>
 	</div>
 
 	<input type="text" placeholder="Your name (optional)" name="author" />
@@ -25,6 +43,20 @@
 </form>
 
 <style>
+	#search-container {
+		margin-bottom: 16px;
+		padding: 16px;
+		border-radius: 8px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		background: #110f1b;
+	}
+
+	#search-container input {
+		background: #090811;
+	}
+
 	#images-container {
 		margin: 8px;
 		display: flex;
